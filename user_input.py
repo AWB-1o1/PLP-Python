cfb3bb9ec09d759c7f79f9049d135ac8659e3f1c
AWB-1o1 <159696419+AWB-1o1@users.noreply.github.com>
2024-03-24 08:50:30 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("where are you located? ")
+print ("hey there ", name +  " you are just ", age +  " years old and you live in ", location)
